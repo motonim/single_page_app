fdceab1a5d50bc0de9a30a95f55eb94c87f492fb
Jaeri Park <motonimjrp@gmail.com>
2022-10-14 15:52:02 -0400

    Delete config.js

--- config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-   consumer_key: '7t4IjvtiqdNywc3SFo6yCjdLj',
-   consumer_secret: 'zUzswI6hU2Mjk1QQOT17VH7niuPxtGvw5RXbFUtOhPtHFDliGY',
-   access_token: '1572325928096645123-LEba64hnq9tCOpPIpjSl4wZboAvy5Z',
-   access_token_secret:'K2c94Ais9kwuvlQcIwbC9fwWjpXG1SI9vwpF5lKvi3Uu6'
-}
